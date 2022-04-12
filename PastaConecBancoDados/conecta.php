2aa6efd58de104b489c6e04132c7f1c27e70bf6c
AlunoTI <AlunoTI@estacio.acad>
2022-04-12 09:48:04 -0300

    commit do conecta php

---
+++ PastaConecBancoDados/conecta.php
@@ -0,0 +1,9 @@
+<?php
+
+//$pdo = new PDO('mysql:host=localhost;dbname=contatinhos', 'root', Null);
+
+$pdo = new PDO('mysql:host=danielgomes.pro.br;dbname=danielgo_contato', 'danielgo_alunos', 'estacio2022');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+?>
+
